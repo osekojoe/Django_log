77cdc520f4384c4b43e0a74cb9123c5075813697
Joe <osekojoe@gmail.com>
2020-11-20 23:55:43 +0100

    User entry

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -4,6 +4,12 @@ from . import views
 
 app_name = 'learning_logs'
 urlpatterns = [
-     #Home
-     path('', views.index, name = 'index')
+     #Home page
+     path('', views.index, name = 'index'),
+     #page displaying all topics
+     path('topics/', views.topics, name = 'topics' ),
+     # Details page for a single topic.
+     path('topics/<int:topic_id>/', views.topic, name= 'topic'),
+     #page for adding a new topic
+     path('new_topic/', views.new_topic, name = 'new_topic'),
 ]
